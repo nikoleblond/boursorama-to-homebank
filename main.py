e77faa5f33eb7768142640e96ef4819788784a50
Nicolas <nicolas.bouzin@tutanota.com>
2021-12-01 21:34:27 +0100

    correction date de l'opération + assignation automatique PRLV SEPA

--- main.py
+++ main.py
@@ -4,13 +4,15 @@ madataframe = pandas.read_csv("export-operations.csv", encoding="cp1252", delimi
 madataframe["payment"] = ""
 madataframe.loc[madataframe["label"].str.contains("VIR"), ["payment"]] = 4
 madataframe.loc[madataframe["label"].str.contains("CB"), ["payment"]] = 1
+madataframe.loc[madataframe["label"].str.contains("CARTE"), ["payment"]] = 1
+madataframe.loc[madataframe["label"].str.contains("PRLV SEPA"), ["payment"]] = 10
 madataframe["info"] = madataframe["label"]
-madataframe = madataframe.drop(columns="dateOp")
+madataframe = madataframe.drop(columns="dateVal")
 madataframe = madataframe.drop(columns="categoryParent")
 madataframe = madataframe.drop(columns="accountbalance")
 madataframe = madataframe.drop(columns="accountNum")
 madataframe = madataframe.drop(columns="accountLabel")
-madataframe = madataframe.rename(columns={"dateVal": "date"})
+madataframe = madataframe.rename(columns={"dateOp": "date"})
 madataframe["memo"] = ""
 madataframe["tags"] = ""
 madataframe["payee"] = ""
