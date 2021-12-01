f1bf49f864e4497994f32d71d69427847dc40252
Nicolas <nicolas.bouzin@tutanota.com>
2021-12-02 00:05:08 +0100

    correction carte débit (index 5 au lieu de 1)

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ import pandas
 madataframe = pandas.read_csv("export-operations.csv", encoding="cp1252", delimiter=";")
 madataframe["payment"] = ""
 madataframe.loc[madataframe["label"].str.contains("VIR"), ["payment"]] = 4
-madataframe.loc[madataframe["label"].str.contains("CB"), ["payment"]] = 1
-madataframe.loc[madataframe["label"].str.contains("CARTE"), ["payment"]] = 1
+madataframe.loc[madataframe["label"].str.contains("CB"), ["payment"]] = 5
+madataframe.loc[madataframe["label"].str.contains("CARTE"), ["payment"]] = 5
 madataframe.loc[madataframe["label"].str.contains("PRLV SEPA"), ["payment"]] = 10
 madataframe["info"] = madataframe["label"]
 madataframe = madataframe.drop(columns="dateVal")
